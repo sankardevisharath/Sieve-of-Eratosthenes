a0add32b374eebf245a2b596b97133d6690519b5
sharath <sankardevisharath@gmail.com>
2011-12-29 10:06:15 +0530

    comments added and variable names made more meaningful

--- Sieve_of_Eratosthenes.c
+++ Sieve_of_Eratosthenes.c
@@ -3,25 +3,34 @@
 int main(int argc,char *argv[])
 	{
 	if(argc<2)
-		printf(" Enter argument \n");
+		printf("No argument passed.. \n");
 	else 
 		{
+		//n is the upper limit of prime numbers
+		//sample input : ./a.out 10
+		//sample output: 2 3 5 7
 		int n,i,k,j;
 		n=atoi(argv[1]);
-		printf(" %d \t", n);
-		int numarray[(n/32) +1];
+		int number_status[(n/32) +1];
+		//numarray initialized to 0.
 		for(i=0;i<=n/32;i++)
-			numarray[i/32]=0;
-		printf("array initialized\n");
+			number_status[i/32]=0;
+		//i is initialized to first prime that is 2.
 		i=2;
 		for(;i<=n;i++)
 			{
-			if(((numarray[i/32]) & (1<<i))==0)
+			//condition inside if becomes true, if i is a prime number. 
+			//for example, if i=10, then number_status[10/32] & (1<<10) 
+			//becomes zero. That means 10 th bit of number_status[0] is 0,
+			//ie it is not marked.
+			if(((number_status[i/32]) & (1<<i))==0)
 				{
+				//print i as prime number. and mark its corresponding 
+				//multiples less than n.
 				printf(" %d ", i);
 				j=2*i;k=3;
 				for(;j<=n;j=i*k,k++)
-					numarray[j/32] |= 1<<j;
+					number_status[j/32] |= 1<<j;
 				}
 			}
 		printf(" \n");
